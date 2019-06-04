d976cc0d90770ca62ebb2f9a919e632dd2eb3dbc
Robin Gareus <robin@gareus.org>
2019-06-04 13:54:39 +0200

    NO-OP: whitespace and braces (clang format)

--- common_ltcdump.c
+++ common_ltcdump.c
@@ -1,14 +1,15 @@
 #include "common_ltcdump.h"
 
-void print_user_bits(FILE *outfile, const LTCFrame *f) {
-  int user_bits  = f->user1;
-  user_bits     += f->user2 * 16 <<  0;
-  user_bits     += f->user3 * 16 <<  4;
-  user_bits     += f->user4 * 16 <<  8;
-  user_bits     += f->user5 * 16 << 12;
-  user_bits     += f->user6 * 16 << 16;
-  user_bits     += f->user7 * 16 << 20;
-  user_bits     += f->user8 * 16 << 24;
-  fprintf(outfile, "%08x" "%-3s", user_bits, "");
+void
+print_user_bits (FILE* outfile, const LTCFrame* f)
+{
+	int user_bits = f->user1;
+	user_bits += f->user2 * 16 << 0;
+	user_bits += f->user3 * 16 << 4;
+	user_bits += f->user4 * 16 << 8;
+	user_bits += f->user5 * 16 << 12;
+	user_bits += f->user6 * 16 << 16;
+	user_bits += f->user7 * 16 << 20;
+	user_bits += f->user8 * 16 << 24;
+	fprintf (outfile, "%08x" "%-3s", user_bits, "");
 }
-/* vi:set ts=8 sts=2 sw=2: */
